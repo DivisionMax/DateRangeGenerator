78472990520b4f1eeab32a65032cead96760da42
Gavin Wiener <gavin.w@superbalist.com>
2018-05-28 20:50:43 +0200

    Generate 3 months gaps. Remove leading whitespace

--- date_range_generator.py
+++ date_range_generator.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-six_months = relativedelta(months=+6)
+six_months = relativedelta(months=+3)
 start_date = date(2005, 1, 1)
 end_date = start_date + six_months
 today = date.today()
@@ -16,11 +16,11 @@ with open('dates.txt', 'w') as date_file:
 		start_formatted = datetime.strftime(start_date, formatter)
 		end_formatted = datetime.strftime(end_date, formatter)
 		logger.info('{} to {}'.format(start_formatted, end_formatted))
-		date_file.write('{}, {}\n'.format(start_formatted, end_formatted))
+		date_file.write('{},{}\n'.format(start_formatted, end_formatted))
 		start_date = end_date + timedelta(days=1)
 		end_date = start_date + six_months
 	start_formatted = datetime.strftime(start_date, formatter)
 	end_formatted = datetime.strftime(today, formatter)
 	logger.info('{} to {}'.format(start_formatted, end_formatted))
-	date_file.write('{}, {}\n'.format(start_formatted, end_formatted))
+	date_file.write('{},{}\n'.format(start_formatted, end_formatted))
 
